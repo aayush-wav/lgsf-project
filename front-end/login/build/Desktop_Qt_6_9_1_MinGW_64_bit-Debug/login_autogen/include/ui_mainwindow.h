f66255a09dbc12a1243b7ce8e1d0d5a7740e2147
aayush-wav <kcaayush0007@gmail.com>
2025-07-17 10:56:31 +0545

    Revert "changes to main gui"
    This reverts commit 88b6b3c10210d035c239a32f988a149414ef1a1d.

--- front-end/login/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/login_autogen/include/ui_mainwindow.h
+++ front-end/login/build/Desktop_Qt_6_9_1_MinGW_64_bit-Debug/login_autogen/include/ui_mainwindow.h
@@ -11,8 +11,11 @@
 
 #include <QtCore/QVariant>
 #include <QtWidgets/QApplication>
+#include <QtWidgets/QLabel>
+#include <QtWidgets/QLineEdit>
 #include <QtWidgets/QMainWindow>
 #include <QtWidgets/QMenuBar>
+#include <QtWidgets/QPushButton>
 #include <QtWidgets/QStatusBar>
 #include <QtWidgets/QWidget>
 
@@ -22,6 +25,11 @@ class Ui_MainWindow
 {
 public:
     QWidget *centralwidget;
+    QPushButton *loginBtn;
+    QLineEdit *passwordLineEdit;
+    QLineEdit *usernameLineEdit;
+    QLabel *labelBadapatra;
+    QLabel *labelNewUser;
     QMenuBar *menubar;
     QStatusBar *statusbar;
 
@@ -29,18 +37,68 @@ public:
     {
         if (MainWindow->objectName().isEmpty())
             MainWindow->setObjectName("MainWindow");
-        MainWindow->resize(800, 600);
+        MainWindow->resize(1123, 651);
+        QFont font;
+        font.setFamilies({QString::fromUtf8("montserrat")});
+        font.setPointSize(16);
+        font.setKerning(false);
+        MainWindow->setFont(font);
+        MainWindow->setAcceptDrops(true);
+        MainWindow->setAutoFillBackground(false);
+        MainWindow->setStyleSheet(QString::fromUtf8("*{\n"
+"	background-image: url(:/res/background.png);\n"
+"	background-position: center;\n"
+"	background-size:100% 100%;\n"
+"	font-family: montserrat;\n"
+"	background-color:  #1C1C1C;\n"
+"}\n"
+""));
         centralwidget = new QWidget(MainWindow);
         centralwidget->setObjectName("centralwidget");
-        centralwidget->setStyleSheet(QString::fromUtf8("background-image: url(:/images/bg.jpg);\n"
-"background-repeat: no-repeat;\n"
+        centralwidget->setStyleSheet(QString::fromUtf8("background-image: url(:/res/background.png);\n"
 "background-position: center;\n"
-"background-size: cover;\n"
+"background-size: 100% 100%;\n"
+"background-repeat: no-repeat;\n"
+"background: transparent;\n"
 ""));
+        loginBtn = new QPushButton(centralwidget);
+        loginBtn->setObjectName("loginBtn");
+        loginBtn->setGeometry(QRect(640, 483, 300, 45));
+        QFont font1;
+        font1.setFamilies({QString::fromUtf8("montserrat")});
+        font1.setPointSize(12);
+        font1.setBold(true);
+        font1.setKerning(false);
+        loginBtn->setFont(font1);
+        loginBtn->setStyleSheet(QString::fromUtf8("background-color: rgb(255, 255, 255);\n"
+"border-radius: 4;\n"
+"color: #2148C0;"));
+        passwordLineEdit = new QLineEdit(centralwidget);
+        passwordLineEdit->setObjectName("passwordLineEdit");
+        passwordLineEdit->setGeometry(QRect(640, 395, 300, 45));
+        usernameLineEdit = new QLineEdit(centralwidget);
+        usernameLineEdit->setObjectName("usernameLineEdit");
+        usernameLineEdit->setGeometry(QRect(640, 330, 300, 45));
+        labelBadapatra = new QLabel(centralwidget);
+        labelBadapatra->setObjectName("labelBadapatra");
+        labelBadapatra->setGeometry(QRect(640, 208, 345, 111));
+        QFont font2;
+        font2.setFamilies({QString::fromUtf8("montserrat")});
+        font2.setPointSize(22);
+        font2.setBold(true);
+        font2.setKerning(false);
+        labelBadapatra->setFont(font2);
+        labelBadapatra->setStyleSheet(QString::fromUtf8("text-alignment: center;"));
+        labelBadapatra->setAlignment(Qt::AlignmentFlag::AlignCenter);
+        labelNewUser = new QLabel(centralwidget);
+        labelNewUser->setObjectName("labelNewUser");
+        labelNewUser->setGeometry(QRect(640, 450, 300, 20));
+        labelNewUser->setCursor(QCursor(Qt::CursorShape::PointingHandCursor));
+        labelNewUser->setAlignment(Qt::AlignmentFlag::AlignCenter);
         MainWindow->setCentralWidget(centralwidget);
         menubar = new QMenuBar(MainWindow);
         menubar->setObjectName("menubar");
-        menubar->setGeometry(QRect(0, 0, 800, 27));
+        menubar->setGeometry(QRect(0, 0, 1123, 25));
         MainWindow->setMenuBar(menubar);
         statusbar = new QStatusBar(MainWindow);
         statusbar->setObjectName("statusbar");
@@ -54,6 +112,9 @@ public:
     void retranslateUi(QMainWindow *MainWindow)
     {
         MainWindow->setWindowTitle(QCoreApplication::translate("MainWindow", "MainWindow", nullptr));
+        loginBtn->setText(QCoreApplication::translate("MainWindow", "LOGIN", nullptr));
+        labelBadapatra->setText(QCoreApplication::translate("MainWindow", "\340\244\210 - BADAPATRA", nullptr));
+        labelNewUser->setText(QCoreApplication::translate("MainWindow", "New User?", nullptr));
     } // retranslateUi
 
 };
--- front-end/login/mainwindow.h
+++ front-end/login/mainwindow.h
@@ -2,11 +2,11 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QEvent>
+#include <QLineEdit>
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +17,21 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+protected:
+    bool eventFilter(QObject *obj, QEvent *event) override;
+
+private slots:
+    void on_pushButton_Login_clicked();
+
 private:
     Ui::MainWindow *ui;
+
+    QLineEdit *passwordLineEdit;
+    QAction *togglePasswordAction;
+    bool passwordVisible = false;
+
+    void setupUI();
+    void togglePasswordVisibility();
 };
+
 #endif // MAINWINDOW_H
