f29af3ad8db940d81550c795a287f2bc751bd1fb
Dalton Khatri <khatridalton1235@gmail.com>
2025-07-28 00:02:31 +0545

    Dbhandler Updated for signup

--- front-end/login/dbhandler.h
+++ front-end/login/dbhandler.h
@@ -1,37 +1,35 @@
 #ifndef DBHANDLER_H
 #define DBHANDLER_H
 
-#include <QtSql>
-#include <QDebug>
-#include <QCryptographicHash>
-#include <QMessageBox>
+#include <QSqlDatabase>
+#include <QSqlQuery>
+#include <QSqlError>
 #include <QSettings>
+#include <QMessageBox>
+#include <QCryptographicHash>
 
-class DBHandler
-{
+struct DatabaseConfig {
+    QString hostName;
+    QString databaseName;
+    QString userName;
+    QString password;
+    int port;
+};
+
+class DBHandler {
 public:
     static DBHandler& instance();
-
     bool connectToDB();
-    QString hashPassword(const QString &password);
     bool createUser(const QString &username, const QString &password);
     bool authenticateUser(const QString &username, const QString &password);
+    bool usernameExists(const QString &username);
+    QString hashPassword(const QString &password);
 
 private:
     DBHandler();
-    DBHandler(const DBHandler&) = delete;
-    void operator=(const DBHandler&) = delete;
-
-    QSqlDatabase m_db;
-    struct DatabaseConfig {
-        QString hostName;
-        QString databaseName;
-        QString userName;
-        QString password;
-        int port;
-    };
-    DatabaseConfig dbConfig;
     bool loadDatabaseConfig();
+    QSqlDatabase m_db;
+    DatabaseConfig dbConfig;
 };
 
 #endif // DBHANDLER_H
