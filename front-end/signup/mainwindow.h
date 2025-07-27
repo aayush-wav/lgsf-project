22b8902c6baa4e453e1324ee346ec7d33aad8793
sid-thapa-11 <thapasiddharthh19@gmail.com>
2025-07-28 00:40:28 +0545

    Updated signup

--- front-end/signup/mainwindow.h
+++ front-end/signup/mainwindow.h
@@ -2,11 +2,16 @@
 #define MAINWINDOW_H
 
 #include <QMainWindow>
+#include <QLineEdit>
+#include <QPushButton>
+#include <QLabel>
+#include <QMessageBox>
+#include <QCryptographicHash>
+#include <QDebug>
+#include "dbhandler.h"
 
 QT_BEGIN_NAMESPACE
-namespace Ui {
-class MainWindow;
-}
+namespace Ui { class MainWindow; }
 QT_END_NAMESPACE
 
 class MainWindow : public QMainWindow
@@ -17,7 +22,17 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+private slots:
+    void on_signupBtn_clicked();
+    void on_labelAlreadyHaveAccount_linkActivated(const QString &link);
+
 private:
     Ui::MainWindow *ui;
+    void setupUI();
+    bool validateInputs();
+    QString hashPassword(const QString &password);
+    bool checkUsernameExists(const QString &username);
+    void openLoginPage();
 };
+
 #endif // MAINWINDOW_H
